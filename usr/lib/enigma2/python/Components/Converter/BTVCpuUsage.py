784ce74284d585f349add1bd756c7c32e437f21c
persianpros <persianpros@yahoo.com>
2020-04-27 18:53:33 +0430

    Correct order of the beginning of py files

--- usr/lib/enigma2/python/Components/Converter/BTVCpuUsage.py
+++ usr/lib/enigma2/python/Components/Converter/BTVCpuUsage.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 from Converter import Converter
 from Poll import Poll
--- usr/lib/enigma2/python/Components/Renderer/DRRunningText.py
+++ usr/lib/enigma2/python/Components/Renderer/DRRunningText.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 from Renderer import Renderer
 from skin import parseColor, parseFont
